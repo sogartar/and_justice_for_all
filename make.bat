e28404d609f20fa21a0bc15921aefaa9fc62d8af
Sogartar <sogartary@yahoo.com>
2021-04-27 10:09:52 -0700

    Bump minor version to 0.2.0

--- make.bat
+++ make.bat
@@ -4,7 +4,7 @@ SET SCRIPT_DIR=%~dp0
 SET WORKING_DIR=%cd%
 
 cd "%SCRIPT_DIR%"
-7z a "%WORKING_DIR%\mod_and_justice_for_all0.1.0.zip" scripts || goto error
+7z a "%WORKING_DIR%\mod_and_justice_for_all0.2.0.zip" scripts || goto error
 exit 0
 
 :error
